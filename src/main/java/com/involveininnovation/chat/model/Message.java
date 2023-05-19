2ea0865c64a3d21b15e070d2335c461884181fdb
vaishrachit <vaishrachit87@gmail.com>
2023-05-19 18:49:51 +0530

    First Change

---
+++ src/main/java/com/involveininnovation/chat/model/Message.java
@@ -0,0 +1,63 @@
+package com.involveininnovation.chat.model;
+
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+    
+    
+    
+    
+	public Message(String senderName, String receiverName, String message, String date, Status status) {
+		super();
+		this.senderName = senderName;
+		this.receiverName = receiverName;
+		this.message = message;
+		this.date = date;
+		this.status = status;
+	}
+	
+	public String getSenderName() {
+		return senderName;
+	}
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+	public String getReceiverName() {
+		return receiverName;
+	}
+	public void setReceiverName(String receiverName) {
+		this.receiverName = receiverName;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+    
+    
+}
