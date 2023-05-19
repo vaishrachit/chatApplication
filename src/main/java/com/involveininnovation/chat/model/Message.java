c3bcdd7b268fea17dc21a8fb92f7cf7102586148
vaishrachit <vaishrachit87@gmail.com>
2023-05-19 19:19:43 +0530

    Removed getter setter

--- src/main/java/com/involveininnovation/chat/model/Message.java
+++ src/main/java/com/involveininnovation/chat/model/Message.java
@@ -16,48 +16,4 @@ public class Message {
     private String date;
     private Status status;
     
-    
-    
-    
-	public Message(String senderName, String receiverName, String message, String date, Status status) {
-		super();
-		this.senderName = senderName;
-		this.receiverName = receiverName;
-		this.message = message;
-		this.date = date;
-		this.status = status;
-	}
-	
-	public String getSenderName() {
-		return senderName;
-	}
-	public void setSenderName(String senderName) {
-		this.senderName = senderName;
-	}
-	public String getReceiverName() {
-		return receiverName;
-	}
-	public void setReceiverName(String receiverName) {
-		this.receiverName = receiverName;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public Status getStatus() {
-		return status;
-	}
-	public void setStatus(Status status) {
-		this.status = status;
-	}
-    
-    
 }
